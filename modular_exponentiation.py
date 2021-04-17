a324ad54685ad0c1d4c948bdbc1da2f0c27af58e
herhor <awegrzyn17@gmail.com>
2021-04-18 00:26:58 +0200

    Added general-purpose function for modular exponentiation.

--- modular_exponentiation.py
+++ modular_exponentiation.py
@@ -1,3 +1,5 @@
+import base_representations as br
+
 
 def modular_exponentiation_special_case(b, k, m):
     """
@@ -18,4 +20,23 @@ def modular_exponentiation_special_case(b, k, m):
             print("c after ", i, "-th iteration: ", c)
         return c
 
-# modular_exponentiation_special_case(b=3, k=8, m=7)
+
+def modular_exponentiation(b, e, m):
+    # 1. find binary representation of the exponent e
+    bin_rep_list = br.find_binary_number_representation_indep(e)
+    # 2. calculate the required powers of 2
+    list_powers_of_two = list(range(0, len(bin_rep_list)))
+    list_modular_powers = [0] * len(list_powers_of_two)
+    for i in range(0, len(list_powers_of_two)):
+        if i == 0:
+            list_modular_powers[i] = b % m
+        else:
+            list_modular_powers[i] = ((list_modular_powers[i-1]) * (list_modular_powers[i-1])) % m
+    # 3. use the powers of 2 to calculate the modular exponent
+    c = 1
+    for i in range(0, len(bin_rep_list)):
+        if bin_rep_list[i] == 1:
+            c = (list_modular_powers[i] * c) % m
+    return c
+
+
